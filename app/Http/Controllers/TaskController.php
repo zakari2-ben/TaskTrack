bbfb5bc74c5a6fc9e059b688eff2bc688eb27d06
zakari2-ben <riariazakaria6@gmail.com>
2025-11-19 00:24:16 +0100

    Improve code cleanliness: use FormRequest for validation in controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,64 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\updateTaskRequest;
 use App\Models\Task;
 use GuzzleHttp\Psr7\Request as Psr7Request;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 
 class TaskController extends Controller
 {
     //get informations :
-    public function index(){
+    public function index()
+    {
         $task = Task::all();
-        return response()->json($task,200);
+        return response()->json($task, 200);
     }
 
     //set a task :
-    public function store (Request $request){
-
-        $validatedate = $request->validate(
-            [
-                'title'=>'required|string|max:60',
-                'description'=>'nullable|string',
-                'priority'=>'required|integer|min:1|max:5',
-            ]
-            );
-
-            $task = Task::create($validatedate);
-            return response()->json($task, 201);
+    public function store(StoreTaskRequest $request)
+    {
+        $task = Task::create($request->Validated());
+        return response()->json($task, 201);
     }
 
     //update a task :
-    public function update(Request $request,$id) {
-
+    public function update(updateTaskRequest $request, $id)
+    {
         $task = Task::findorFail($id);
-        $validatedate = $request->validate(
-            [
-                'title'=>'sometimes|required|string|max:60',
-                'description'=>'sometimes|nullable|string',
-                'priority'=>'sometimes|required|integer|min:1|max:5',
-            ]
-            );
-        $task->update($validatedate);
-        return response()->json($task,200);
-
+        $task->update($request->validated());
+        return response()->json($task, 200);
     }
 
     //show tasks : 
-    public function show($id) {
+    public function show($id)
+    {
 
         $task = Task::findorFail($id);
-        return response()->json($task,200);
+        return response()->json($task, 200);
     }
 
     //delet task :
-    public function destroy($id) {
+    public function destroy($id)
+    {
 
         $task = Task::findorFail($id);
-        $task -> delete();
-        return response()->json(null,204 );
-
+        $task->delete();
+        return response()->json(null, 204);
     }
-
 }
---
+++ app/Http/Requests/updateTaskRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class updateTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'sometimes|required|string|max:60',
+            'description' => 'sometimes|nullable|string',
+            'priority' => 'sometimes|required|integer|min:1|max:5'
+        ];
+    }
+}
